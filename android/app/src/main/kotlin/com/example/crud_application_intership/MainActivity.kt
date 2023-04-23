f2f3d641d4bb74ebd7025e3dfdfcaa9b175d019b
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-04-23 09:32:29 +0530

    Default App

---
+++ android/app/src/main/kotlin/com/example/crud_application_intership/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crud_application_intership
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomeScreen(),
+    );
+  }
+}
+
+class MyHomeScreen extends StatelessWidget {
+  const MyHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}
